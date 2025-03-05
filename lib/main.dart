3853ee3f3b7f26bcc059e34ca83852b154d47461
NNNXT <74697758+NNNXT@users.noreply.github.com>
2025-03-05 11:17:50 +0700

    :sparkles: add data to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bsru_firestoree/firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -41,16 +42,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -58,66 +49,88 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void _addCountry() async {
+    await FirebaseFirestore.instance.collection('country').add({
+      "name": "กรีซ",
+      "enName": "Greece",
+      "alpha2": "GR",
+      "image": 'https://flagcdn.com/w320/gr.png',
     });
+  }
+
+  void _addMultiCountry() async {
+    var country = [
+      {
+        "name": "กรีซ",
+        "enName": "Greece",
+        "alpha2": "GR",
+        "alpha3": "GRC",
+        "numeric": "300",
+        "iso3166_2": "ISO 3166-2:GR",
+      },
+      {
+        "name": "กรีนแลนด์",
+        "enName": "Greenland",
+        "alpha2": "GL",
+        "alpha3": "GRL",
+        "numeric": "304",
+        "iso3166_2": "ISO 3166-2:GL",
+      },
+      {
+        "name": "กวม",
+        "enName": "Guam",
+        "alpha2": "GU",
+        "alpha3": "GUM",
+        "numeric": "316",
+        "iso3166_2": "ISO 3166-2:GU",
+      },
+      {
+        "name": "กัมพูชา",
+        "enName": "Cambodia",
+        "alpha2": "KH",
+        "alpha3": "KHM",
+        "numeric": "116",
+        "iso3166_2": "ISO 3166-2:KH",
+      },
+      {
+        "name": "กัวเดอลุป",
+        "enName": "Guadeloupe",
+        "alpha2": "GP",
+        "alpha3": "GLP",
+        "numeric": "312",
+        "iso3166_2": "ISO 3166-2:GP",
+      },
+    ];
+
+    for (int i = 0; i < country.length; i++) {
+      await FirebaseFirestore.instance.collection('country').add({
+        "name": country[i]['name'],
+        "enName": country[i]['enName'],
+        "alpha2": country[i]['alpha2'],
+        "image":
+            'https://flagcdn.com/w320/${(country[i]['alpha2'] ?? '').toLowerCase()}.png',
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text('$_counter', style: Theme.of(context).textTheme.headlineMedium),
-          ],
+          children: <Widget>[],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _addMultiCountry,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
